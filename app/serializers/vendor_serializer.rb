d6c9b3711a08a91631b96b4bf26e051159ab402b
Chris Asercion <chris.asercion@gmail.com>
2023-09-11 08:10:30 -0600

    work

--- app/serializers/vendor_serializer.rb
+++ app/serializers/vendor_serializer.rb
@@ -1,6 +1,10 @@
 class VendorSerializer
   include JSONAPI::Serializer
-  attributes :name, :description, :contact_name, :contact_phone, :credit_accepted
+  attributes :name, :description, :contact_name, :contact_phone, :credit_accepted#, :states_sold_in
 
   has_many :markets
+
+  def states_sold_in
+    require 'pry'; binding.pry
+  end
 end
